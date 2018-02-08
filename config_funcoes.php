4cd7e2205526f445563d8455a50b688dff41dfb1
Luis Fernando Batels <luisfbatels@gmail.com>
2018-02-08 08:32:32 -0200

    Exemplos para higienização

---
+++ config_funcoes.php
@@ -0,0 +1,78 @@
+<?php
+
+require "vendor/autoload.php";
+
+
+/**
+ * Valida e higieniza os valores do array
+ *
+ * @param array $aDados - Array a ser processado, por exemplo, $_POST ou $_GET
+ * @param array $aValidacoes - Validações a serem aplicadas em cada campo
+ * @param array $aFiltros - Filtros a serem aplicados em cada campo
+ *
+ * @return Object
+ */
+function fHigienizaInput($aDados, $aValidacoes = [], $aFiltros = []) {
+  $oGump = new GUMP();
+
+  $aDados = $oGump->sanitize($aDados);
+
+  if (!empty($aValidacoes))
+    $oGump->validation_rules($aValidacoes);
+
+  if (!empty($aFiltros))
+    $oGump->filter_rules($aFiltros);
+
+  $oRun = $oGump->run($aDados);
+  $oReturn = new StdClass();
+
+  if ($oRun === false) {
+    $oReturn->bOk = false;
+    $oReturn->aErros = $oGump->get_errors_array();
+  } else {
+    $oReturn->bOk = true;
+    $oReturn->aResultado = $oRun;
+  }
+
+  return $oReturn;
+}
+
+/**
+ * Valida e higieniza a super variável $_POST. Retorna um
+ * boleano conforme o sucesso do processo
+ *
+ * @param array $aValidacoes - Validações a serem aplicadas em cada campo
+ * @param array $aFiltros - Filtros a serem aplicados em cada campo
+ * @return boolean
+ */
+function fHigienizaPost($aValidacoes = [], $aFiltros = []) {
+
+  $oRet = fHigienizaInput($_POST, $aValidacoes, $aFiltros);
+
+  if ($oRet->bOk)
+    $_POST = $oRet->aResultado;
+  else
+    $_POST = $oRet->aErros;
+
+  return $oRet->bOk;
+}
+
+/**
+ * Valida e higieniza a super variável $_GET. Retorna um
+ * boleano conforme o sucesso do processo
+ *
+ * @param array $aValidacoes - Validações a serem aplicadas em cada campo
+ * @param array $aFiltros - Filtros a serem aplicados em cada campo
+ * @return boolean
+ */
+function fHigienizaGet($aValidacoes = [], $aFiltros = []) {
+
+  $oRet = fHigienizaInput($_GET, $aValidacoes, $aFiltros);
+
+  if ($oRet->bOk)
+    $_GET = $oRet->aResultado;
+  else
+    $_GET = $oRet->aErros;
+
+  return $oRet->bOk;
+}
---
+++ tests/config_funcoes_test.php
@@ -0,0 +1,55 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class ConfigFuncoesTest extends TestCase {
+
+  public function testSanidade() {
+
+    $oTmp = fHigienizaInput([
+      'a' => ' ab<b>c</b>'
+      ], false, [
+      'a' => 'trim|sanitize_string'
+      ]);
+
+    $this->assertEquals('abc', $oTmp->aResultado['a']);
+
+  }
+
+  public function testPost() {
+
+    $_POST = ['a' => 'ab<b>c</c> ']; //Simulamos a post
+
+    $this->assertTrue(fHigienizaPost(false, [
+      'a' => 'trim|sanitize_string'
+    ]));
+
+    $this->assertEquals('abc', $_POST['a']);
+
+  }
+
+  public function testGet() {
+
+    $_GET = ['a' => 'ab<b>c</c> ']; //Simulamos a get
+
+    $this->assertTrue(fHigienizaGet(false, [
+      'a' => 'trim|sanitize_string'
+    ]));
+
+    $this->assertEquals('abc', $_GET['a']);
+
+  }
+
+  public function testGetError() {
+
+    $_GET = ['a' => 'ab<b>c</c> ']; //Simulamos a get
+
+    $this->assertFalse(fHigienizaGet([
+      'a' => 'required|integer'
+    ]));
+
+    $this->assertEquals('The A field must be a number without a decimal', $_GET['a']);
+
+  }
+
+}
